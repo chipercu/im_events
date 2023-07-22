a9696d2a8f7041718cbbeb9aad2dd6b2c080c6e3
a.kiperku <a.kiperku@infomaximum.com>
2023-07-22 15:18:20 +0300

    fix:cros2

--- src/main/java/com/infomaximum/im_event/Config/SecurityConfig.java
+++ src/main/java/com/infomaximum/im_event/Config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.infomaximum.im_event.Config;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.io.IOException;
 
 /**
  * Created by a.kiperku
@@ -10,11 +14,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  */
 
 @Configuration
-public class SecurityConfig extends WebMvcConfigurationSupport  {
+public class SecurityConfig implements Filter {
+
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://192.168.89.115:8180")
-                .allowedMethods("*");
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(request, res);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
     }
 }
